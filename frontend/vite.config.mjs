6f86922af2bf813d5f6e5dc3b1d94d64b7aa3c09
Saksham Rana <sakshamrana101@gmail.com>
2026-01-10 00:49:19 +0530

    fix: CORS error

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -9,14 +9,6 @@ export default defineConfig({
     host: "0.0.0.0",
     strictPort: true,
     watch: { usePolling: true },
-    proxy: {
-      // in docker-compose dev we want browser requests to /api -> backend service
-      "/api": {
-        target: "http://backend:8000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
   },
   build: { outDir: "dist" },
 });
